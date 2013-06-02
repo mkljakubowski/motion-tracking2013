f493f6b6f0357fab8b4411f0cd885b360c47b6ce
Mikołaj Jakubowski <mkl.jakubowski@gmail.com>
2013-06-02 10:43:21 +0200

    fixed motion racking

--- app/assets/javascripts/demo.js
+++ app/assets/javascripts/demo.js
@@ -1,90 +1,115 @@
-var DEMO = function(){};
+var DEMO = function () {
+};
 
-DEMO.prototype.start = function() {
+DEMO.prototype.start = function () {
     var that = this;
-
-    this.tracker = new HT.Tracker( {fast: true} );
 
     this.video = document.getElementById("video");
     this.canvas = document.getElementById("canvas");
+    this.cameraCanvas = document.getElementById("camera");
     this.context = this.canvas.getContext("2d");
-    this.context.scale(1, -1);
-
-    this.canvas.width = parseInt(this.canvas.style.width) / 2;
-    this.canvas.height = parseInt(this.canvas.style.height) / 2;
-
-    that.image = that.context.createImageData(that.canvas.width * 0.2, that.canvas.height * 0.2);
+    this.cameraCtx = this.cameraCanvas.getContext("2d");
+    this.oldImage = undefined;
+    this.oldFiltered = undefined;
 
     navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia;
-    if (navigator.getUserMedia){
-        navigator.getUserMedia({video:true},
-            function(stream){ return that.videoReady(stream); },
-            function(error){ return that.videoError(error); } );
+    if (navigator.getUserMedia) {
+        navigator.getUserMedia({video: true},
+            function (stream) {
+                return that.videoReady(stream);
+            },
+            function (error) {
+                return that.videoError(error);
+            });
     }
-    this.middle = {x:0,y:0};
 };
 
-DEMO.prototype.videoReady = function(stream){
+DEMO.prototype.videoReady = function (stream) {
     if (window.webkitURL) {
         this.video.src = window.webkitURL.createObjectURL(stream);
     } else {
         this.video.src = stream;
     }
-
     this.tick();
 };
 
-DEMO.prototype.videoError = function(error){ };
+DEMO.prototype.videoError = function (error) {
+};
 
-DEMO.prototype.tick = function(){
+DEMO.prototype.tick = function () {
     var that = this, image, candidate;
 
-    requestAnimationFrame( function() { return that.tick(); } );
+    requestAnimationFrame(function () {
+        return that.tick();
+    });
 
-    if (this.video.readyState === this.video.HAVE_ENOUGH_DATA){
+    if (this.video.readyState === this.video.HAVE_ENOUGH_DATA) {
         image = this.snapshot();
-        candidate = this.tracker.detect(image);
-        this.draw(candidate);
+        this.draw(this.filter(image));
     }
 };
 
-DEMO.prototype.snapshot = function(){
-    this.context.drawImage(this.video, 0, 0, this.canvas.width, this.canvas.height);
-    return this.context.getImageData(0, 0, this.canvas.width, this.canvas.height);
+DEMO.prototype.snapshot = function () {
+    this.cameraCtx.drawImage(this.video, 0, 0, this.cameraCanvas.width, this.cameraCanvas.height);
+    return this.cameraCtx.getImageData(0, 0, this.cameraCanvas.width, this.cameraCanvas.height);
 };
 
-DEMO.prototype.draw = function(candidate){
-    if(candidate){
-        this.context.putImageData(
-        this.createImage(this.tracker.mask, this.image),
-        this.canvas.width - this.image.width,
-        this.canvas.height - this.image.height);
-
+DEMO.prototype.draw = function (filtered) {
+    if (filtered) {
+        this.context.putImageData(filtered, 0, 0);
     }
 };
 
+DEMO.prototype.filter = function (image) {
+    if (this.oldImage) {
+        var data = image.data, output = this.cameraCtx.createImageData(image.width, image.height), i = 0, r, g, b, diff = 0;
+        for (i = 0; i < image.data.length; i += 4) {
+            r = Math.abs(data[i] - this.oldImage.data[i]);
+            g = Math.abs(data[i+1] - this.oldImage.data[i+1]);
+            b = Math.abs(data[i+2] - this.oldImage.data[i+2]);
+            if (r + g + b > 50) {
+                output.data[i] = 255;
+                output.data[i+1] = 255;
+                output.data[i+2] = 255;
+                output.data[i+3] = 255;
+                diff++;
+            }else{
+                output.data[i] = 0;
+                output.data[i+1] = 0;
+                output.data[i+2] = 0;
+                output.data[i+3] = 255;
+            }
+        }
+        this.oldImage = image;
+        if(diff > 500 || !this.oldFiltered){
+            this.oldFiltered = output;
+            return output;
+        }else{
+            return this.oldFiltered;
+        }
+    } else {
+        this.oldImage = image;
+        return image;
+    }
+};
 
-DEMO.prototype.createImage = function(imageSrc, imageDst){
+DEMO.prototype.createImage = function (imageSrc, imageDst) {
     var src = imageSrc.data, dst = imageDst.data,
         width = imageSrc.width, span = 4 * width,
         len = src.length, i = 0, j = 0, k = 0;
 
-    for(i = 0; i < len; i += span){
-
-        for(j = 0; j < width; j += 5){
-
+    for (i = 0; i < len; i += span) {
+        for (j = 0; j < width; j += 5) {
             dst[k] = dst[k + 1] = dst[k + 2] = src[i];
             dst[k + 3] = 255;
             k += 4;
-
             i += 5;
         }
     }
-
     return imageDst;
 };
 
-window.onload = function(){
+window.onload = function () {
     var demo = new DEMO();
     demo.start();
 };
--- public/javascripts/vendor/handtracking.js
+++ public/javascripts/vendor/handtracking.js
@@ -1,49 +1,33 @@
-
 var HT = HT || {};
 
-HT.Tracker = function(){
-
-  this.mask = new CV.Image();
-  this.skinner = new HT.Skinner();
-
+HT.Tracker = function () {
+    this.mask = new CV.Image();
+    this.skinner = new HT.Skinner();
 };
 
-HT.Tracker.prototype.detect = function(image){
-  if( !this.image_old) this.image_old=image
-  this.skinner.maskmove(image,this.image_old, this.mask);
-  this.image_old=image
-
-
-  a=this.mask
-  return this.mask
+HT.Tracker.prototype.detect = function (image) {
+    if (!this.image_old) this.image_old = image;
+    this.skinner.maskmove(image, this.image_old, this.mask);
+    this.image_old = image;
+    return this.mask;
 };
 
+HT.Skinner = function () { };
 
-
-HT.Skinner = function(){
-};
-
-
-HT.Skinner.prototype.maskmove = function(oldSrc,imageSrc, imageDst){
+HT.Skinner.prototype.maskmove = function (oldSrc, imageSrc, imageDst) {
     var src = imageSrc.data, old = oldSrc.data, dst = imageDst.data, len = src.length,
         i = 0, j = 0,
         dr, dg, db, value;
 
-    for(; i < len; i += 4){
-        dr = Math.abs(src[i]-old[i]);
-        dg = Math.abs(src[i+1]-old[i+1]);
-        db = Math.abs(src[i+2]-old[i+2]);
-
+    for (; i < len; i += 4) {
+        dr = Math.abs(src[i] - old[i]);
+        dg = Math.abs(src[i + 1] - old[i + 1]);
+        db = Math.abs(src[i + 2] - old[i + 2]);
 
         value = 0;
-
-        if (dr+dg+db >50){
-                        value=255
-        }
-
-
-
-        dst[j ++] = value;
+        if (dr + dg + db > 50)
+            value = 255;
+        dst[j++] = value;
     }
 
     imageDst.width = imageSrc.width;
@@ -51,4 +35,3 @@ HT.Skinner.prototype.maskmove = function(oldSrc,imageSrc, imageDst){
 
     return imageDst;
 };
-
